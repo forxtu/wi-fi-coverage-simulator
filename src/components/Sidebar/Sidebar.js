27ebf76b3b0c13dde5df615cc1d49f7bd39f75da
forxtu <forxtu@gmail.com>
2018-06-18 00:05:20 +0200

    feat(sidebar): changes, additional controls

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,22 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import TxPower from './TxPower/TxPower';
 import RadioOptions from './RadioOptions/RadioOptions';
+import Button from 'components/Button/Button';
 
 import './Sidebar.css';
 
 const Sidebar = props => {
   return (
     <div className="Sidebar">
-      <TxPower txPower={props.txPower}>TX Power</TxPower>
-      <RadioOptions radioOptions={props.radioOptions}>Radio</RadioOptions>
+      <TxPower
+        txPower={props.txPower}
+        handlePowerChange={props.handlePowerChange}
+        selectedPower={props.selectedPower}
+      >
+        TX Power
+      </TxPower>
+      <RadioOptions
+        radioOptions={props.radioOptions}
+        handleRadioChange={props.handleRadioChange}
+        selectedRadio={props.selectedRadio}
+      >
+        Radio
+      </RadioOptions>
+      <div className="Sidebar__controls">
+        <Button btnType="Button__save" handleSave={props.handleSave}>
+          Save
+        </Button>
+        <Button btnType="Button__cancel" handleCancel={props.handleCancel}>
+          Cancel
+        </Button>
+      </div>
     </div>
   );
 };
 
 Sidebar.propTypes = {
-  txPower: PropTypes.string,
-  radioOptions: PropTypes.arrayOf(PropTypes.string)
+  txPower: PropTypes.arrayOf(PropTypes.object),
+  radioOptions: PropTypes.arrayOf(PropTypes.object),
+  handlePowerChange: PropTypes.func,
+  handleRadioChange: PropTypes.func,
+  handleSave: PropTypes.func,
+  handleCancel: PropTypes.func,
+  selectedPower: PropTypes.string,
+  selectedRadio: PropTypes.string
 };
 
 export default Sidebar;
