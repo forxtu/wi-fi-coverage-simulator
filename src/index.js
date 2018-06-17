41e7f8a4ad713b0b9b1a508c181cde77b0d152b7
forxtu <forxtu@gmail.com>
2018-06-17 07:09:05 +0200

    feat(index): add webfont loader and fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Lato:100,300,400,700,900', 'sans-serif']
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
