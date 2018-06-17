729f5255631f4b776ab6ce9c0ae21883fbda2f7c
forxtu <forxtu@gmail.com>
2018-06-18 00:05:59 +0200

    feat(radio): add basic functinality

--- src/components/Sidebar/RadioOptions/RadioOptions.js
+++ src/components/Sidebar/RadioOptions/RadioOptions.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Aux from 'hoc/Aux';
+
+import '../Sidebar.css';
+import './RadioOptions.css';
+
 const RadioOptions = props => {
+  const radioOptionsList = props.radioOptions.map(radioOption => (
+    <li
+      key={`${radioOption.power}_radio`}
+      className="RadioOptions__items-group"
+      onChange={props.handleRadioChange}
+    >
+      <input
+        className="RadioOptions__items-input"
+        name="radioOption"
+        type="radio"
+        value={radioOption.power}
+        id={`${radioOption.power}_radio_id`}
+        // checked={radioOption.power === props.selectedRadio}
+        defaultChecked={radioOption.power === props.selectedRadio}
+      />
+      <label
+        className="RadioOptions__items-label"
+        htmlFor={`${radioOption.power}_radio_id`}
+      >
+        {radioOption.power}
+      </label>
+
+      <div className="RadioOptions__items-check" />
+    </li>
+  ));
+
   return (
     <div className="RadioOptions">
-      <h2>{props.children}</h2>
-      <h1> {props.radioOptions} </h1>
+      <h2 className="Sidebar__title">{props.children}</h2>
+      <ul className="RadioOptions__items-wrap">{radioOptionsList}</ul>
     </div>
   );
 };
 
 RadioOptions.propTypes = {
   children: PropTypes.node,
-  radioOptions: PropTypes.arrayOf(PropTypes.string)
+  radioOptions: PropTypes.arrayOf(PropTypes.object),
+  handleRadioChange: PropTypes.func,
+  selectedRadio: PropTypes.string
 };
 
 export default RadioOptions;
