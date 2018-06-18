6b4323e6297f562811f159b09f6cab8b0e994c4f
forxtu <forxtu@gmail.com>
2018-06-19 01:42:17 +0200

    feat(antenna): draggable, rebuild to class

--- src/components/AntennaField/Antenna/Antenna.js
+++ src/components/AntennaField/Antenna/Antenna.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Draggable from 'react-draggable';
 
 import './Antenna.css';
 
-const Antenna = props => {
-  const AntennaArea = {
-    border: `${props.calculatedDistance}px solid #d2d8e0`
-  };
-  return (
-    <div className="Antenna">
-      <div className="Antenna__item" style={AntennaArea} />
-    </div>
-  );
-};
+const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
+// const {deltaPosition} = this.state;
+class Antenna extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // AntennaAreaWidth: `border: ${this.props.calculatedDistance}px solid #d2d8e0`
+      deltaPosition: {
+        x: 0,
+        y: 0
+      }
+    };
+    this.AntennaArea = {
+      border: `${this.props.calculatedDistance}px solid #d2d8e0`
+    };
+
+    this.handleDrag = this.handleDrag.bind(this);
+  }
+
+  handleDrag(e, ui) {
+    const {x, y} = this.state.deltaPosition;
+    this.setState({
+      deltaPosition: {
+        x: x + ui.deltaX,
+        y: y + ui.deltaY
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Draggable bounds="parent" onDrag={this.handleDrag} {...dragHandlers}>
+        <div className="Antenna">
+        <div>x: {this.state.deltaPosition.x.toFixed(0)}, y: {this.state.deltaPosition.y.toFixed(0)}</div>
+          <div className="Antenna__item" style={this.AntennaArea}>
+            <div className="Antenna__item-point" />
+          </div>
+        </div>
+      </Draggable>
+    );
+  }
+}
 
 Antenna.propTypes = {
   children: PropTypes.node,
