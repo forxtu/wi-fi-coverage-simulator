7542dc4bd7bb101993385767db7eba01caa2cf86
forxtu <forxtu@gmail.com>
2018-06-26 14:49:39 +0200

    feat(antenna): update distance after props change

--- src/components/AntennaField/Antenna/Antenna.js
+++ src/components/AntennaField/Antenna/Antenna.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { any } from 'prop-types';
 import Draggable from 'react-draggable';
 
 import './Antenna.css';
 
 const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
-// const {deltaPosition} = this.state;
 class Antenna extends Component {
+  static propTypes = {
+    calculatedDistance: any
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -37,17 +40,17 @@ class Antenna extends Component {
   }
 
   distanceBetweenClientsAndAccesPoint() {
-    var getPositionAtCenter = function(element) {
-      var data = element.getBoundingClientRect();
+    let getPositionAtCenter = function(element) {
+      let data = element.getBoundingClientRect();
       return {
         x: data.left + data.width / 2,
         y: data.top + data.height / 2
       };
     };
 
-    var getDistanceBetweenElements = function(a, b) {
-      var aPosition = getPositionAtCenter(a);
-      var bPosition = getPositionAtCenter(b);
+    let getDistanceBetweenElements = function(a, b) {
+      let aPosition = getPositionAtCenter(a);
+      let bPosition = getPositionAtCenter(b);
 
       return Math.sqrt(
         Math.pow(aPosition.x - bPosition.x, 2) +
@@ -57,37 +60,30 @@ class Antenna extends Component {
 
     let clientsItems = document.querySelectorAll('.Clients__item');
     clientsItems = Array.prototype.slice.call(clientsItems);
-    // console.log(clientsItems);
     clientsItems.map(client => {
-      // console.log(item);
-      // return item;
-      var distance = getDistanceBetweenElements(
+      let distance = getDistanceBetweenElements(
         document.querySelector('.Antenna'),
         client
       );
 
-      distance - 44 < this.props.calculatedDistance ? client.style.backgroundColor = '#24B77D' : client.style.backgroundColor = '#FC3516';
+      distance - 44 < this.props.calculatedDistance
+        ? (client.style.backgroundColor = '#24B77D')
+        : (client.style.backgroundColor = '#FC3516');
 
-      // if (distance - 44 < this.props.calculatedDistance) {
-      //   // this.props.activateClientAccess();
-      //   // client.style.backgroundColor = this.props.isClientActive;
-      //   client.style.backgroundColor = '#24B77D';
-      // } else {
-      //   // this.props.disableClientAccess();
-      //   // client.style.backgroundColor = this.props.isClientActive;
-      //   client.style.backgroundColor = '#FC3516';
-      // }
       return distance;
     });
-
-    // console.log(this.props.calculatedDistance);
-    // console.log(distance);
   }
 
   componentDidMount() {
     setTimeout(() => {
       this.distanceBetweenClientsAndAccesPoint();
     }, 0);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.calculatedDistance !== prevProps.calculatedDistance) {
+      this.distanceBetweenClientsAndAccesPoint();
+    }
   }
 
   render() {
@@ -112,11 +108,5 @@ class Antenna extends Component {
     );
   }
 }
-
-Antenna.propTypes = {
-  children: PropTypes.node,
-  calculatedDistance: PropTypes.any,
-  toggleClientAccess: PropTypes.func
-};
 
 export default Antenna;
